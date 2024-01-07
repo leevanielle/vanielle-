d895786cff2fa03a4fc3eb1b66cf2d4ae5823451
Vanielle Lee <vanielle@odecloud.com>
2024-01-07 19:16:47 +0900

    Devops: fixed deployment conflicts with astro vercel packages

--- astro.config.ts
+++ astro.config.ts
@@ -3,12 +3,20 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
+import vercel from "@astrojs/vercel/serverless";
 import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
+  output: "hybrid",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+    maxDuration: 8,
+  }),
   integrations: [
     tailwind({
       applyBaseStyles: false,
